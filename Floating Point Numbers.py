9fdafe6726fb2bfaff2706aacae695a61256b08f
NewtonCaleb <55808052+NewtonCaleb@users.noreply.github.com>
2021-02-05 20:33:05 -0600

    Update Floating Point Numbers.py
    Added comments

--- Floating Point Numbers.py
+++ Floating Point Numbers.py
@@ -1,14 +1,15 @@
+# Import the random library
 import random
 
+# Set variables a, b, and c to random floating-point values
 a = random.random()
 b = random.random()
 c = random.random()
 
-
+# Print out those values
 print(a)
 print(b)
 print(c)
 
-
+# Printing out whether the following is true or false
 print(a+(b+c) == (a+b)+c)
-
