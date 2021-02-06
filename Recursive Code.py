3045305105188710b142aa20f0d7d8f4eccf138f
Ginny (Khue) Dang <58489398+ginny100@users.noreply.github.com>
2021-02-05 20:30:04 -0600

    Update Recursive Code.py

--- Recursive Code.py
+++ Recursive Code.py
@@ -1,9 +1,12 @@
-def fib1(n=10):
+def fib1(n = 10):
+    # Base cases
     if n == 0:
         return 0
     if n == 1:
         return 1
+    
+    # Normal case
     return fib1(n-1) + fib1(n-2)
 
-
+# Driver Code
 print(fib1(45))
